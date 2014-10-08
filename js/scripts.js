6041952cb302cd8ceb8f316d06a01b5b0e1a0f2d
jabbb <bertsche>
2014-10-07 21:05:43 -0700

    sadfasdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
-$("img").load(function() {
+$(function() {
     var imgNum = 4;
-    $(document).scrollTop($(document).height());
+    $("img").ready(function() {
+        $(document).scrollTop($(document).height());
 
-    function addImgs() {
-        if (imgNum < 44) {
-            console.log("adding");
-            var position0 = $(document).height() - $(document).scrollTop();
-            alert(position0);
-            //for (var i = 0; i < 3; i++) {
+        function addImgs() {
+            if (imgNum < 44) {
+                console.log("adding");
+                var position0 = $(document).height() - $(document).scrollTop();
+                alert(position0);
+                //for (var i = 0; i < 3; i++) {
                 var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
                 $("#main").prepend(newImg).hide();
                 imgNum++;
-            //}
-            $(newImg).load(function() {
-                $(newImg).show();
-                $(document).scrollTop($(document).height() - position0);
-            });
+                //}
+                $(newImg).ready(function() {
+                    $(newImg).show();
+                    $(document).scrollTop($(document).height() - position0);
+                });
+            }
         }
-    }
 
-    $(document).scroll(function() {
-        if ($(document).scrollTop() == 0) {
-            //toLoad = false
-            console.log($(document).scrollTop());
-            addImgs();
-        }
+        $(document).scroll(function() {
+            if ($(document).scrollTop() == 0) {
+                //toLoad = false
+                console.log($(document).scrollTop());
+                addImgs();
+            }
+        });
+
     });
 
 });
