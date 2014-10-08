b0753c0783325f5a998fd389f5e1bd258bc5ad78
jabbb <bertsche>
2014-10-08 02:28:07 -0700

    sdfsdf

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ $(function() {
 
     function addImgs() {
         if (imgNum < 47) {
-            console.log("adding");
 
             var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>").hide();
             $("#main").prepend(newImg);
@@ -22,9 +21,8 @@ $(function() {
     }
 
     $(document).scroll(function() {
-        if ($(document).scrollTop() == 0) {
+        if ($(document).scrollTop() < 1000) {
             //toLoad = false
-            console.log($(document).scrollTop());
             addImgs();
         }
     });
