45a3b22c83494366079d91d32f0ba794f1d9115c
jabbb <bertsche>
2014-10-08 02:22:32 -0700

    sdfsdfsdf

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,17 @@ $(function() {
     $("#startingBlock").css('height', $(window).height() + 'px');
     addImgs();
 
-    /*$("<img src='img/1.jpg' alt='1'>").load(function() {
-        var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
-        $("#main").prepend(newImg);
-        $(newImg).hide();
-        imgNum++;
-
-        $(newImg).load(function() {
-            $(newImg).show();
-            $(document).scrollTop($(document).height());
-        });
-    });*/
-
     function addImgs() {
         if (imgNum < 47) {
             console.log("adding");
-            var position0 = $(document).height() - $(document).scrollTop();
 
-            var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+            var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>").hide();
             $("#main").prepend(newImg);
-            $(newImg).hide();
+            //$(newImg).hide();
             imgNum++;
 
             $(newImg).load(function() {
+            var position0 = $(document).height() - $(document).scrollTop();
                 $(newImg).show();
                 $(document).scrollTop($(document).height() - position0);
             });
