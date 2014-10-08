1cc8fb9903436bacd284e247aefcba7063605a25
jabbb <bertsche>
2014-10-07 21:36:22 -0700

    efsf

--- js/scripts3.js
+++ js/scripts3.js
@@ -1,25 +1,24 @@
 $(function() {
     var imgNum = 4;
-    $("img").load(function() {
+    /*$("img").load(function() {
         $(document).scrollTop($(document).height());
-    });
+    });*/
 
-    function addImgs() {
+    function loadImg() {
         if (imgNum < 44) {
             console.log("adding");
-            var position0 = $(document).height() - $(document).scrollTop();
-
             var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
             $("#main").prepend(newImg);
             $(newImg).hide();
             imgNum++;
-            
-            $(newImg).load(function() {
-                $(newImg).show();
-                $(document).scrollTop($(document).height() - position0);
-            });
         }
     }
+
+    $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>").load(function() {
+        var position0 = $(document).height() - $(document).scrollTop();
+        $(this).show();
+        $(document).scrollTop($(document).height() - position0);
+    });
 
     $(document).scroll(function() {
         if ($(document).scrollTop() == 0) {
