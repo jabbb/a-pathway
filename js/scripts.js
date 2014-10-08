3d136a4d92b046e6f6994d933a9dbf811b32f562
jabbb <bertsche>
2014-10-07 18:32:56 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(function() {
+    var imgNum = 4;
+    $(document).scrollTop($(document).height());
+
+    function addImgs() {
+        if (imgNum < 44) {
+            console.log("adding");
+            var position0 = $(document).height() - $(document).scrollTop();
+            for (var i = 0; i < 3; i++) {
+                var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+                $("#main").prepend(newImg);
+                imgNum++;
+            }
+            $(newImg).ready(function() {
+                $(document).scrollTop($(document).height() - position0);
+            });
+        }
+    }
+
+    $(document).scroll(function() {
+        if ($(document).scrollTop() == 0) {
+            toLoad = false
+            console.log($(document).scrollTop());
+            addImgs();
+        }
+    });
+
+});
