0bd6d279d2e2f1106ea0272825e71df3134a3911
jabbb <bertsche>
2014-10-07 20:52:21 -0700

    sdfsf

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,20 @@ $(function() {
             console.log("adding");
             var position0 = $(document).height() - $(document).scrollTop();
             alert(position0);
-            for (var i = 0; i < 3; i++) {
+            //for (var i = 0; i < 3; i++) {
                 var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
                 $("#main").prepend(newImg);
                 imgNum++;
-            }
-            //$(newImg).ready(function() {
+            //}
+            $(newImg).ready(function() {
                 $(document).scrollTop($(document).height() - position0);
-            //});
+            });
         }
     }
 
     $(document).scroll(function() {
         if ($(document).scrollTop() == 0) {
-            toLoad = false
+            //toLoad = false
             console.log($(document).scrollTop());
             addImgs();
         }
