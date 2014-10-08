d078806ec7a7a4546aea3a8ced194f5180529305
jabbb <bertsche>
2014-10-07 20:49:16 -0700

    fsfdsdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,27 @@
 $(function() {
+    var imgNum = 4;
     $(document).scrollTop($(document).height());
-    var imgNum = 2;
 
     function addImgs() {
         if (imgNum < 44) {
             console.log("adding");
             var position0 = $(document).height() - $(document).scrollTop();
-            var images = [];
+            alert(position0);
             for (var i = 0; i < 3; i++) {
-                images[i] = new Image();
-                images[i].src = "img/" + imgNum + ".jpg"
+                var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+                $("#main").prepend(newImg);
                 imgNum++;
+
+                $(newImg).ready(function() {
+                    $(document).scrollTop($(document).height() - position0);
+                });
             }
-            for (var i = 0; i < 3; i++) {
-                //var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
-                $("#main").prepend(images[i]);
-            }
-            //$(newImg).ready(function() {
-                $(document).scrollTop($(document).height() - position0);
-            //});
         }
     }
 
     $(document).scroll(function() {
         if ($(document).scrollTop() == 0) {
+            toLoad = false
             console.log($(document).scrollTop());
             addImgs();
         }
