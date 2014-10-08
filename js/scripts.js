9cc81a6aeffaf9668bbe3faf5c6a20036058b1e4
jabbb <bertsche>
2014-10-07 21:18:33 -0700

    sfsdfsd

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,31 @@ $(function() {
         $(document).scrollTop($(document).height());
     });
 
+    var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+    $("#main").prepend(newImg);
+    $(newImg).hide();
+    imgNum++;
+
     function addImgs() {
         if (imgNum < 44) {
             console.log("adding");
             var position0 = $(document).height() - $(document).scrollTop();
             //alert(position0);
             //for (var i = 0; i < 3; i++) {
-            var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
-            $("#main").prepend(newImg);
-            $(newImg).hide();
-            imgNum++;
             //}
             $(newImg).load(function() {
                 $(newImg).show();
                 $(document).scrollTop($(document).height() - position0);
             });
+            var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+            $("#main").prepend(newImg);
+            $(newImg).hide();
+            imgNum++;
         }
     }
 
     $(document).scroll(function() {
-        if ($(document).scrollTop() > 1000) {
+        if ($(document).scrollTop() == 0) {
             //toLoad = false
             console.log($(document).scrollTop());
             addImgs();
