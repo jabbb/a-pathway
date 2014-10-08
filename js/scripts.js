98577d35e3501c7932625f801316eea6c019b100
jabbb <bertsche>
2014-10-07 20:46:00 -0700

    fsdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(function() {
     $(document).scrollTop($(document).height());
     var imgNum = 2;
-    addImgs()
 
     function addImgs() {
-        //if (imgNum < 44) {
+        if (imgNum < 44) {
+            console.log("adding");
             var position0 = $(document).height() - $(document).scrollTop();
-            for (var i = 2; i < 46; i++) {
-            var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
-            $("#main").prepend(newImg);
-            imgNum++;
+            var images = [];
+            for (var i = 0; i < 3; i++) {
+                images[i] = new Image();
+                images[i].src = "img/" + imgNum + ".jpg"
+                imgNum++;
             }
-            $(newImg).ready(function() {
+            for (var i = 0; i < 3; i++) {
+                //var newImg = $("<img src='img/" + imgNum + ".jpg' alt='" + imgNum + "'>");
+                $("#main").prepend(images[i]);
+            }
+            //$(newImg).ready(function() {
                 $(document).scrollTop($(document).height() - position0);
-            });
-        //}
+            //});
+        }
     }
 
-    /*$(document).scroll(function() {
+    $(document).scroll(function() {
         if ($(document).scrollTop() == 0) {
+            console.log($(document).scrollTop());
             addImgs();
         }
-    });*/
+    });
 
 });
